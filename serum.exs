f6a03a379ebdf2722b9750bd6c69c21f49423767
SajalSood <sajal.sood@gmail.com>
2020-12-28 13:06:35 -0500

    serum fix

--- serum.exs
+++ serum.exs
@@ -3,7 +3,7 @@
   site_description: "A bit about me !!!",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
-  author: "Sajal Sood,
+  author: "Sajal Sood",
   author_email: "john.doe@example.com",
   plugins: [
     {Serum.Plugins.LiveReloader, only: :dev}
